cdfdf50b9b10e245e04b9eb7d4ea4a1a39b208d6
Andy <skandla2002@gmail.com>
2016-10-07 00:29:43 +0900

    [20161006] requireJS 사용시 테스트 시나리오 추가 및 오류 사항 적용

---
+++ src/java/WebApp/requirejs/test/test-main.js
@@ -0,0 +1,35 @@
+var allTestFiles = []
+var TEST_REGEXP = /(spec|test)\.js$/i
+
+// Get a list of all the test files to include
+Object.keys(window.__karma__.files).forEach(function (file) {
+  if (TEST_REGEXP.test(file)) {
+    // Normalize paths to RequireJS module names.
+    // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
+    // then do not normalize the paths
+    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '')
+    allTestFiles.push(normalizedTestModule)
+  }
+})
+
+require.config({
+	// Karma serves files from '/base'
+    baseUrl: '/base/src',
+
+    paths: {
+        'jquery': '../lib/jquery',
+        'underscore': '../lib/underscore',
+    },
+
+    shim: {
+        'underscore': {
+            exports: '_'
+        }
+    },
+
+    // ask Require.js to load these files (all our tests)
+    deps: tests,
+
+    // start test run, once Require.js is done
+    callback: window.__karma__.start
+})
